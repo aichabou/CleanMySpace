518094d9a873657f16e9351c0891ca8e34833312
Aicha Bouchaikh <aicha.bouchaikh@gmail.com>
2025-07-31 15:31:14 +0200

    feat: add cros to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Autoriser les origines spécifiques
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    # ajoute ici ton futur domaine déployé si besoin
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
